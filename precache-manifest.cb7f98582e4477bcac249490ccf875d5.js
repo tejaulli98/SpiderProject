20e875fef9f701faa33c13024169b4b50a855620
tejaulli <teja.ulli123@gmail.com>
2020-09-29 14:12:02 +0530

    Updates

---
+++ precache-manifest.cb7f98582e4477bcac249490ccf875d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f3582c3fefb925560d08e038aed7c41",
+    "url": "/SpiderProject/index.html"
+  },
+  {
+    "revision": "84a596e421b7949b0b6c",
+    "url": "/SpiderProject/static/css/main.007df983.chunk.css"
+  },
+  {
+    "revision": "cc66073056294c144704",
+    "url": "/SpiderProject/static/js/2.60d1cff0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SpiderProject/static/js/2.60d1cff0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84a596e421b7949b0b6c",
+    "url": "/SpiderProject/static/js/main.af567dbd.chunk.js"
+  },
+  {
+    "revision": "53c1750c16c7d752455b",
+    "url": "/SpiderProject/static/js/runtime-main.a481d299.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/SpiderProject/static/media/logo.ee7cd8ed.svg"
+  }
+]);

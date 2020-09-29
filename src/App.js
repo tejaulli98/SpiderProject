87ffdacccb8b90167b2be7f0ed372c62d49a3acc
tejaulli <teja.ulli123@gmail.com>
2020-09-29 15:50:19 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { HashRouter, Route, Switch } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header.js'
@@ -9,11 +8,10 @@ import Footer from './Footer.js'
 function App() {
   return (
     <div className="App">
-     <HashRouter basename={process.env.PUBLIC_URL}>
+     
       <Header />
       <GoAuto />
       <Footer />
-	  </HashRouter>
     </div>
   );
 }
